b5bf71d3424265ebeb11feb7c7686c4924f09822
laurentnunez <lnunez@hotmail.fr>
2023-03-22 16:09:38 +0100

    connexion form and controled fields ok

--- userConnect.php
+++ userConnect.php
@@ -1,13 +1,31 @@
+<?php include 'inc/header.php'?>
+
+<?php
+  if(!empty($_POST)){
+
+    if(isset($_POST["email"], $_POST["userpassword"])
+
+    && !empty($_POST["email"]) && !empty($_POST["userpassword"])
+    
+    ){
+      //le formulaire est complet
+    }
+    else{
+      die("le formulaire est incomplet");
+    }
+
+
+  }
+
+?>
 
 <div class="user-container">
-    <form class="d-flex flex-column"  method="post" action="../inc/connect.php">
-        <div class="col-10">
-          <input type="text" placeholder="utilisateur" name="username" class="form-control">
-        </div>
-          <input type="text" placeholder="mot de passe" name="userpassword" class="form-control">
-        </div>
-        <div class="col-1">
-          <button type="submit" class="btn btn-warning" style="font-size:16px;">Se Connecter</button>
-        </div>
+    <form class="d-flex flex-column" method="post" action="">
+        
+          <input type="email" placeholder="email" name="email" class="form-control">
+       
+          <input type="password" placeholder="mot de passe" name="userpassword" class="form-control mt-1">
+          <button type="submit" class="btn btn-warning mt-1" style="font-size:16px;">Connexion</button>   
+        
     </form>
 </div>
