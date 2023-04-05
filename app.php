aa15f056d8395d6aee92046dd3066ecd97204316
laurentnunez <lnunez@hotmail.fr>
2023-04-05 11:34:00 +0200

    change css off the task priority and algo of conditionnal change color

--- app.php
+++ app.php
@@ -16,17 +16,27 @@ session_start();
 
 <div class="tasks_container">
   <?php $taskCounter=0 ?>
-
+  
   <!--On parcourt la liste des tâches venant de la bdd grâce à la boucle foreach-->
   <?php foreach ($tasksList as $taskData) { ?>
           
     <!--On affiche les taches dans une liste-->   
-          
-    <form class="task alert alert-light d-flex justify-content-between d-flex align-content-center" method="post" action="inc/save.php" role="alert" style="box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;">
+    
+    
+      <form class="callout callout-warning d-flex justify-content-between"
+        <?php if($taskData['id_priority']== 1){?>
+        style="border-left-color:#d9534f;"
+        
+        <?php } if($taskData['id_priority']== 2){?>
+        style="border-left-color:#f0ad4e;"
+
+        <?php } if($taskData['id_priority']== 3){?>
+        style="border-left-color:#5cb85c;"<?php }?>
+        >
 
        <?php 
        if($taskData['status']== 2){?>
-        <div class="task-data d-flex align-content-center">
+        <div class="col-9 task-data d-flex align-content-center">
                 
           <a class="btn" style="color:green; padding:0px;" type="submit" href="inc/update.php?id=<?= $taskData['id']?>&action=nochecked"><ion-icon name="checkmark-circle" size="large"></ion-icon></a>
 
@@ -36,30 +46,19 @@ session_start();
         <div class="buttons d-flex justify-content-around">
               
       <?php } else { ?>
-        <div class="task-data">
+        <div class="col-9 task-data">
 
           <a class="btn" style="color:green; padding:0px;" type="submit" href="inc/update.php?id=<?= $taskData['id']?>&action=checked"><ion-icon size="large" name="checkmark-circle-outline"></ion-icon></a>
 
-          <input type="text" name="taskname"  style="border:none;padding-bottom:0.5rem;" class="form-control form-control-sm" value="<?= $taskData['name'] ?>">             
+          <input type="text" name="taskname"  style="border:none;" class="task form-control" value="<?= $taskData['name'] ?>">             
 
         </div>
 
         <div class="buttons d-flex justify-content-around d-flex align-items-center">
 
-          <?php if($taskData['id_priority']== 1){?>
-            <span class="badge rounded-pill text-bg-danger" style="font-size:10px; height:13px; margin-right:0.5rem;"> </span>
-
-            <?php } if($taskData['id_priority']== 2){?>
-            <span class="badge rounded-pill text-bg-warning" style="font-size:10px; height:13px; margin-right:0.5rem;"> </span>
-
-            <?php } if($taskData['id_priority']== 3) {?>
-            <span class="badge rounded-pill text-bg-success" style="font-size:10px; height:13px; margin-right:0.5rem;"> </span>
-          <?php }?> 
-
             <button name="save-button" class="btn" style="color:purple; padding:0px;" type="submit" value="<?= $taskData['id']?>"><ion-icon name="save-outline" size="large"></ion-icon></button>       
               
           <?php $taskCounter++; } ?>
-
           
             <a class="btn" style="color:red; padding:0px;" type="submit" href="inc/delete.php?id=<?= $taskData['id']?>"><ion-icon size="large" name="close"></ion-icon></a>
 
@@ -73,4 +72,5 @@ session_start();
     <form class="disconnect_button d-flex justify-content-center" method="post" action="inc/deconnexion.php">
       <button class="disconnect btn btn-danger" style="margin-bottom:1rem;">Déconnexion</button>
     </form>
+    
 <?php require 'inc/footer.php'?>
